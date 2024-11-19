5176463bd04e6a3747f0977dc813ac48e3626b2b
AlexeyMelnikoff <melnikoff8181@gmail.com>
2024-11-19 10:15:13 +0300

    Add files via upload

---
+++ lesson3.py
@@ -0,0 +1,5 @@
+name = "Python"
+homework = 12
+time = 1.5
+lead_time = (1.5 / 12)
+print('Курс:', name, ', всего задач:', homework, ', затрачено часов:', time, ', среднее время выполнения:', lead_time)
