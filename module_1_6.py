ce62251fdefb264809e1aa81ba0c75997061d26c
AlexeyMelnikoff <melnikoff8181@gmail.com>
2024-11-25 13:39:53 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,16 @@
+my_dict = {'Alex' : 1989, 'Kate' : 1993, 'Alina' : 2016, 'Max' : 2019}
+print(my_dict)
+print(my_dict.pop('Alex'))
+my_dict.update({'Sveta' : 1962,
+                'Vladimir' : 1961})
+print(my_dict)
+del my_dict['Vladimir']
+print(my_dict)
+print(my_dict.get('Vladimir'))
+my_set = {1, 'Alex', (1, 3, 5)}
+print(my_set)
+print(my_set.add(2))
+print(my_set.add(3))
+print(my_set)
+print(my_set.discard(1))
+print(my_set)
