8f5d683ca21076cdd0b288a8929850f765bab776
AlexeyMelnikoff <melnikoff8181@gmail.com>
2024-12-10 12:10:58 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,11 @@
+while 1 > 0:
+    number = int(input('Введите число: '))
+    my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+    number_new = 0
+    if number == 0:
+        continue
+    if number >= 2 > 1:
+        print(number)
+    else:
+        print()
+        break
